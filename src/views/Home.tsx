e1a2d892da4280fab59f76e8f5c475e9e80f9492
caio <caio96.lemos@gmail.com>
2025-02-11 15:00:39 -0300

    feat: layout adjustment

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,13 +1,5 @@
 export function Home() {
-  return <div className="mx-auto max-w-7xl py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
-    <div className="text-center">
-      <h2 className="text-lg font-semibold text-primary-600">App Template</h2>
-      <p className="mt-1 text-4xl font-bold tracking-tight text-gray-900 dark:text-gray-100 sm:text-5xl lg:text-6xl">
-        @majapibr/app-template
-      </p>
-      <p className="mx-auto mt-5 max-w-xl text-xl text-gray-500 dark:text-gray-400">
-        Majapi&apos;s app template.
-      </p>
-    </div>
-  </div>;
+  return (
+    <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8"></div>
+  );
 }
--- src/views/layout/layout.tsx
+++ src/views/layout/layout.tsx
@@ -1,15 +1,12 @@
 import Header from "./Header/Header";
 import { ReactNode } from "react";
-import NavigationDrawer from "./Header/NavigationDrawer";
 
 export default function getLayout(children: ReactNode) {
   return (
     <>
       <Header />
-      <NavigationDrawer>
-        <div className="h-16" />
-        {children}
-      </NavigationDrawer>
+      <div className="h-16" />
+      {children}
     </>
   );
 }

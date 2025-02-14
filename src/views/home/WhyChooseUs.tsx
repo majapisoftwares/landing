2c1ac7d88c5fbe831bfe8f8e343ad9fbb632b38d
caio <caio96.lemos@gmail.com>
2025-02-14 13:05:46 -0300

    feat: enhance WhyChooseUs component with additional Accordion items and improved styling

--- src/views/home/WhyChooseUs.tsx
+++ src/views/home/WhyChooseUs.tsx
@@ -1,7 +1,7 @@
 import { ArrowLongRightIcon } from "@heroicons/react/20/solid";
 import Button from "@italodeandra/ui/components/Button";
 import Accordion from "@italodeandra/ui/components/Accordion";
-import { BoltIcon } from "@heroicons/react/24/outline";
+import { BoltIcon, GlobeAltIcon } from "@heroicons/react/24/outline";
 
 export default function WhyChooseUs() {
   return (
@@ -11,7 +11,7 @@ export default function WhyChooseUs() {
           <div className="bg-gradient-to-br from-zinc-100 to-zinc-400 bg-clip-text font-dm text-5xl font-semibold tracking-[-2.88px] text-transparent">
             Why choose us
           </div>
-          <div className="max-w-[506px] self-stretch text-2xl font-normal leading-8 tracking-[-1.44px] text-zinc-400">
+          <div className="max-w-[506px] self-stretch text-xl font-normal leading-8 tracking-[-1.44px] text-zinc-400">
             We combine innovation, agility, and expertise to deliver custom,
             high-performance solutions. With a global presence in Brazil, the
             US, and Turkey, we ensure cutting-edge technology, dedicated
@@ -28,11 +28,12 @@ export default function WhyChooseUs() {
             </Button>
           </div>
         </div>
-        <div className="flex w-full flex-col items-end">
+        <div className="flex w-full flex-col items-end gap-4">
           <Accordion>
             <Accordion.Item
-              triggerClassName="gap-2 items-center font-tight text-2xl border border-zinc-800 dark:bg-[#151315] px-8 py-5"
-              contentClassName="dark:bg-[#151315] border-x border-b rounded-lg -mt-2 border-zinc-800"
+              className="mr-16 w-[350px] rounded-lg border border-zinc-800 dark:bg-zinc-900"
+              triggerClassName="gap-2 items-center dark:bg-zinc-900 dark:hover:bg-zinc-950/30 font-tight text-2xl px-8 py-5"
+              contentClassName="text-center text-zinc-400 text-lg"
               title={
                 <>
                   Agility & Efficiency
@@ -42,7 +43,41 @@ export default function WhyChooseUs() {
                 </>
               }
             >
-              Test 1 a
+              Cutting-edge technology tailored to your business needs.
+            </Accordion.Item>
+          </Accordion>
+          <Accordion>
+            <Accordion.Item
+              className="w-[350px] rounded-lg border border-zinc-800 dark:bg-zinc-900"
+              triggerClassName="gap-2 items-center dark:bg-zinc-900 dark:hover:bg-zinc-950/30 font-tight text-2xl px-8 py-5"
+              contentClassName="text-center text-zinc-400 text-lg"
+              title={
+                <>
+                  Innovative Solutions
+                  <div className="rounded-full bg-white p-2">
+                    <BoltIcon className="h-4 w-4 text-black" />
+                  </div>
+                </>
+              }
+            >
+              Cutting-edge technology tailored to your business needs.
+            </Accordion.Item>
+          </Accordion>
+          <Accordion>
+            <Accordion.Item
+              className="mr-16 w-[350px] rounded-lg border border-zinc-800 dark:bg-zinc-900"
+              triggerClassName="gap-2 items-center dark:bg-zinc-900 dark:hover:bg-zinc-950/30 font-tight text-2xl px-8 py-5"
+              contentClassName="text-center text-zinc-400 text-lg"
+              title={
+                <>
+                  Global Expertise
+                  <div className="rounded-full bg-white p-2">
+                    <GlobeAltIcon className="h-4 w-4 text-black" />
+                  </div>
+                </>
+              }
+            >
+              Cutting-edge technology tailored to your business needs.
             </Accordion.Item>
           </Accordion>
         </div>

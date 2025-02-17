3462d0cd775c16becdafd7ccd796335b83d72a44
caio <caio96.lemos@gmail.com>
2025-02-17 10:07:45 -0300

    feat: integrate Iconify for enhanced icon usage in Services component

--- src/views/home/Services.tsx
+++ src/views/home/Services.tsx
@@ -1,21 +1,25 @@
 import { ComputerDesktopIcon } from "@heroicons/react/20/solid";
 import { SparklesIcon } from "@heroicons/react/24/outline";
+import { Icon } from "@iconify/react/dist/iconify.js";
 
 export default function Sevices() {
   return (
-    <div className="mx-auto mb-10 flex w-full max-w-screen-xl flex-col items-center gap-10">
+    <div className="mx-auto flex w-full max-w-screen-xl flex-col items-center gap-10">
       <div className="flex flex-col items-center gap-4">
         <div className="bg-gradient-to-br from-zinc-100 to-zinc-400 bg-clip-text font-dm text-5xl font-semibold tracking-[-2.88px] text-transparent">
           Our services
         </div>
-        <div className="max-w-[500px] text-center font-dm text-2xl leading-8 tracking-[-1.44px] text-zinc-400">
+        <div className="max-w-[500px] text-center font-dm text-2xl leading-8 tracking-[-1.44px] text-zinc-500">
           From design to programming, we offer agile and efficient services to
           meet your needs.
         </div>
       </div>
       <div className="flex w-full justify-center gap-8">
         <div className="flex h-60 max-w-[340px] flex-col items-center justify-center gap-4 rounded-lg border border-zinc-800 bg-zinc-900 px-4">
-          <ComputerDesktopIcon className="h-16 w-16 rounded-full bg-white p-4 text-black" />
+          <div className="rounded-full bg-white p-4">
+            <ComputerDesktopIcon className="h-8 w-8 text-black" />
+          </div>
+
           <div className="flex flex-col gap-2 text-center">
             <div className="font-tight text-2xl font-semibold text-white">
               Web & App development
@@ -27,7 +31,10 @@ export default function Sevices() {
           </div>
         </div>
         <div className="flex h-60 max-w-[340px] flex-col items-center justify-center gap-4 rounded-lg border border-zinc-800 bg-zinc-900 px-4">
-          <SparklesIcon className="h-16 w-16 scale-x-flip rounded-full bg-white p-4 text-black" />
+          <div className="rounded-full bg-white p-4">
+            <SparklesIcon className="h-8 w-8 text-black" />
+          </div>
+
           <div className="flex flex-col gap-2 text-center">
             <div className="font-tight text-2xl font-semibold text-white">
               UI/UX Design
@@ -38,7 +45,10 @@ export default function Sevices() {
           </div>
         </div>
         <div className="flex h-60 max-w-[340px] flex-col items-center justify-center gap-4 rounded-lg border border-zinc-800 bg-zinc-900 px-4">
-          <ComputerDesktopIcon className="h-16 w-16 rounded-full bg-white p-4 text-black" />
+          <div className="rounded-full bg-white p-4">
+            <Icon icon="nrk:category-active" className="h-8 w-8 text-black" />
+          </div>
+
           <div className="flex flex-col gap-2 text-center">
             <div className="font-tight text-2xl font-semibold text-white">
               Software development

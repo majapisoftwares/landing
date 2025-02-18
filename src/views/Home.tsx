87c876d097a3e1dd455115192d7e32aeb0c501d8
caio <caio96.lemos@gmail.com>
2025-02-18 10:37:33 -0300

    fix: transparent header

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -5,8 +5,8 @@ import Projects from "./home/Projects";
 
 export function Home() {
   return (
-    <div className="w-full bg-zinc-900/80 bg-[url('/bg-fullhd.png')] bg-top bg-repeat-y">
-      <div className="flex w-full flex-col gap-40 bg-[url('/noise.png')] bg-cover bg-top bg-repeat-y py-40">
+    <div className="-mt-20 bg-zinc-900/80 bg-[url('/noise.png')] bg-cover bg-top bg-repeat-y pt-20">
+      <div className="flex w-full flex-col gap-40 bg-[url('/bg-fullhd.png')] bg-top bg-repeat-y py-40">
         <Hero />
         <WhyChooseUs />
         <Services />

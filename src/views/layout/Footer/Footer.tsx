e9d7d5efee26d505631b60d89b227196a1529171
caio <caio96.lemos@gmail.com>
2025-02-18 17:33:42 -0300

    feat: replace logo image with Logo component in Header and Footer, adjust layout

--- src/views/layout/Footer/Footer.tsx
+++ src/views/layout/Footer/Footer.tsx
@@ -1,8 +1,23 @@
+import Link from "next/link";
+import Logo from "../../logo";
+
 export default function Footer() {
   return (
     <div className="flex w-full border-t border-zinc-700 bg-zinc-900">
-      <div className="mx-auto flex w-full max-w-screen-xl justify-between px-4 py-40">
-        <div>teste</div>
+      <div className="mx-auto flex w-full max-w-screen-xl justify-between px-4 py-20">
+        <div className="flex flex-col gap-4">
+          <Link href="/" className="flex items-center">
+            <Logo className="max-w-32" />
+          </Link>
+          <div className="flex flex-col gap-2">
+            <div className="text-sm font-normal tracking-[-0.1px]">
+              © 2025 Majapi Inc. All rights reserved.
+            </div>
+            <div className="text-sm font-normal tracking-[-0.1px]">
+              Terms & Conditions ∙ Privacy Policy
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/views/layout/Header/Header.tsx
+++ src/views/layout/Header/Header.tsx
@@ -1,19 +1,13 @@
 import Button from "@italodeandra/ui/components/Button";
-import Image from "next/image";
 import Link from "next/link";
+import Logo from "../../logo";
 
 export default function Header() {
   return (
     <div className="flex w-full border-b border-zinc-700 bg-transparent">
       <div className="mx-auto flex w-full max-w-screen-xl justify-between bg-transparent px-4 py-4">
-        <Link href="/">
-          <Image
-            src="/logo-majapi.svg"
-            width={121}
-            height={33}
-            alt="Logo Majapi"
-            className="max-h-8"
-          />
+        <Link href="/" className="flex items-center">
+          <Logo className="max-w-32" />
         </Link>
         <div className="flex gap-4">
           <Button variant="text" className="font-dm text-white">

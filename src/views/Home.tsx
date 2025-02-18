35da9c1947399cf6f0c767d991aa1e4302acd92e
caio <caio96.lemos@gmail.com>
2025-02-18 16:55:19 -0300

    feat: add Contact component to Home view and adjust layout in Footer

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -3,16 +3,19 @@ import WhyChooseUs from "./home/WhyChooseUs";
 import Services from "./home/Services";
 import Projects from "./home/Projects";
 import Clients from "./home/Clients";
+import Contact from "./home/Contact";
 
 export function Home() {
   return (
     <div className="-mt-20 bg-zinc-900/80 bg-[url('/noise.png')] bg-cover bg-top bg-repeat-y pt-20">
-      <div className="flex w-full flex-col gap-40 bg-[url('/bg-fullhd.png')] bg-top bg-repeat-y py-40">
+      <div className="flex w-full flex-col gap-40 bg-[url('/bg-fullhd.png')] bg-top bg-repeat-y pt-40">
         <Hero />
         <WhyChooseUs />
         <Services />
         <Projects />
         <Clients />
+        <Contact />
+        <div className="h-96 bg-gradient-to-t from-zinc-900 to-zinc-900/0 from-30%" />
       </div>
     </div>
   );
--- src/views/layout/Footer/Footer.tsx
+++ src/views/layout/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <div className="flex w-full border-t border-zinc-700 bg-zinc-900/80">
-      <div className="mx-auto flex w-full max-w-screen-xl justify-between bg-transparent px-4 py-40">
+    <div className="flex w-full border-t border-zinc-700 bg-zinc-900">
+      <div className="mx-auto flex w-full max-w-screen-xl justify-between px-4 py-40">
         <div>teste</div>
       </div>
     </div>

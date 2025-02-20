6f8ddb9367ef3480b1d48fa6083d2f75c8f640ee
caio <caio96.lemos@gmail.com>
2025-02-20 10:28:09 -0300

    feat: enhance Footer component with new Solutions sections and social media links

--- src/views/layout/Footer/Footer.tsx
+++ src/views/layout/Footer/Footer.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
 import Logo from "../../logo";
+import Button from "@italodeandra/ui/components/Button";
+import { Icon } from "@iconify/react/dist/iconify.js";
 
 export default function Footer() {
   return (
     <div className="flex w-full border-t border-zinc-700 bg-zinc-900">
-      <div className="mx-auto flex w-full max-w-screen-xl justify-between px-4 py-20">
+      <div className="mx-auto flex w-full max-w-screen-xl items-center justify-between px-4 py-20">
         <div className="flex flex-col gap-4">
           <Link href="/" className="flex items-center">
             <Logo className="max-w-32" />
@@ -16,6 +18,44 @@ export default function Footer() {
             <div className="text-sm font-normal tracking-[-0.1px]">
               Terms & Conditions ∙ Privacy Policy
             </div>
+          </div>
+        </div>
+        <div className="flex flex-col items-center gap-4">
+          <div className="font-tight text-xl font-semibold">Solutions</div>
+          <div className="flex flex-col gap-2">
+            <Button variant="text">Small business</Button>
+            <Button variant="text">Freelancers</Button>
+            <Button variant="text">Costumers</Button>
+            <Button variant="text">Taxes</Button>
+          </div>
+        </div>
+        <div className="flex flex-col items-center gap-4">
+          <div className="font-tight text-xl font-semibold">Solutions</div>
+          <div className="flex flex-col gap-2">
+            <Button variant="text">Small business</Button>
+            <Button variant="text">Freelancers</Button>
+            <Button variant="text">Costumers</Button>
+            <Button variant="text">Taxes</Button>
+          </div>
+        </div>
+        <div className="flex flex-col items-center gap-4">
+          <div className="font-tight text-xl font-semibold">Solutions</div>
+          <div className="flex flex-col gap-2">
+            <Button variant="text">Small business</Button>
+            <Button variant="text">Freelancers</Button>
+            <Button variant="text">Costumers</Button>
+            <Button variant="text">Taxes</Button>
+          </div>
+        </div>
+        <div className="flex flex-col items-center">
+          <div className="font-tight text-xl">Follow us on</div>
+          <div className="flex">
+            <Button target="_blank" variant="text" icon>
+              <Icon icon="mdi:linkedin" className="h-10 w-10" />
+            </Button>
+            <Button target="_blank" variant="text" icon>
+              <Icon icon="mdi:instagram" className="h-10 w-10" />
+            </Button>
           </div>
         </div>
       </div>

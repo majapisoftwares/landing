9294bbac30045245ae3317dc6dbca4a0c2403359
caio <caio96.lemos@gmail.com>
2025-02-20 17:25:28 -0300

    feat: adjust layout spacing and enhance Hero component text size for improved readability

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -8,7 +8,7 @@ import Contact from "./home/Contact";
 export function Home() {
   return (
     <div className="-mt-20 bg-zinc-900/80 bg-[url('/noise.png')] bg-cover bg-top bg-repeat-y pt-20">
-      <div className="flex w-full flex-col gap-40 bg-[url('/bg-fullhd.png')] bg-top bg-repeat-y pt-20 md:pt-40">
+      <div className="flex w-full flex-col gap-20 md:gap-40 bg-[url('/bg-fullhd.png')] bg-top bg-repeat-y pt-20 md:pt-40">
         <Hero />
         <WhyChooseUs />
         <Services />

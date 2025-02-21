4c2cf40202501f78ca2eff565268c197e4b3bf40
caio <caio96.lemos@gmail.com>
2025-02-21 15:02:17 -0300

    feat: improve layout and styling consistency in WhyChooseUs component

--- src/views/home/WhyChooseUs.tsx
+++ src/views/home/WhyChooseUs.tsx
@@ -7,7 +7,7 @@ import { Icon } from "@iconify/react";
 export default function WhyChooseUs() {
   return (
     <div className="mx-auto flex w-full max-w-screen-xl flex-col gap-10 px-4">
-      <div className="flex flex-col items-center gap-12 md:gap-4 md:flex-row">
+      <div className="flex flex-col items-center gap-12 md:flex-row md:gap-4">
         <div className="flex w-full flex-col gap-4 md:gap-10">
           <div className="bg-gradient-to-br from-zinc-100 to-zinc-400 bg-clip-text font-dm text-3xl font-semibold text-transparent md:text-5xl md:tracking-[-2.88px]">
             Why choose us
@@ -29,17 +29,17 @@ export default function WhyChooseUs() {
             </Button>
           </div>
         </div>
-        <div className="flex md:w-full flex-col items-end gap-4">
+        <div className="flex flex-col items-end gap-4 md:w-full">
           <Accordion>
             <Accordion.Item
-              className="md:mr-16 w-[350px] rounded-lg border border-zinc-800 dark:bg-zinc-900"
-              triggerClassName="gap-2 items-center dark:bg-zinc-900 dark:hover:bg-zinc-950/30 font-tight text-xl md:text-2xl px-8 py-5"
+              className="w-[350px] rounded-lg border border-zinc-800 dark:bg-zinc-900 md:mr-16"
+              triggerClassName="gap-2 items-center dark:bg-zinc-900 dark:hover:bg-zinc-950/30 font-tight text-lg md:text-2xl px-8 py-5 flex-row-reverse md:flex-row"
               contentClassName="text-center text-zinc-400 md:text-lg font-dm"
               title={
-                <>
+                <div className="flex gap-3">
                   Agility & Efficiency
                   <BoltIcon className="h-8 w-8 rounded-full bg-white p-2 text-black" />
-                </>
+                </div>
               }
             >
               Cutting-edge technology tailored to your business needs.
@@ -48,16 +48,16 @@ export default function WhyChooseUs() {
           <Accordion>
             <Accordion.Item
               className="w-[350px] rounded-lg border border-zinc-800 dark:bg-zinc-900"
-              triggerClassName="gap-2 items-center dark:bg-zinc-900 dark:hover:bg-zinc-950/30 font-tight text-xl md:text-2xl px-8 py-5 flex-row-reverse md:flex-row"
+              triggerClassName="gap-2 items-center dark:bg-zinc-900 dark:hover:bg-zinc-950/30 font-tight text-lg md:text-2xl px-8 py-5"
               contentClassName="text-center text-zinc-400 md:text-lg font-dm"
               title={
-                <>
+                <div className="flex flex-row-reverse gap-3 md:flex-row">
                   Innovative Solutions
                   <Icon
                     icon="tabler:bulb"
                     className="h-8 w-8 rounded-full bg-white p-2 text-black"
                   />
-                </>
+                </div>
               }
             >
               Cutting-edge technology tailored to your business needs.
@@ -65,14 +65,14 @@ export default function WhyChooseUs() {
           </Accordion>
           <Accordion>
             <Accordion.Item
-              className="md:mr-16 w-[350px] rounded-lg border border-zinc-800 dark:bg-zinc-900"
-              triggerClassName="gap-2 items-center dark:bg-zinc-900 dark:hover:bg-zinc-950/30 font-tight text-xl md:text-2xl px-8 py-5"
+              className="w-[350px] rounded-lg border border-zinc-800 dark:bg-zinc-900 md:mr-16"
+              triggerClassName="gap-2 items-center dark:bg-zinc-900 dark:hover:bg-zinc-950/30 font-tight text-lg md:text-2xl px-8 py-5 flex-row-reverse md:flex-row"
               contentClassName="text-center text-zinc-400 md:text-lg font-dm"
               title={
-                <>
+                <div className="flex gap-3">
                   Global Expertise
                   <GlobeAltIcon className="h-8 w-8 rounded-full bg-white p-2 text-black" />
-                </>
+                </div>
               }
             >
               Cutting-edge technology tailored to your business needs.

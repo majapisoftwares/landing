177d0a68eae768e3ae629442f0abe50e0bdeedef
caio <caio96.lemos@gmail.com>
2025-02-21 17:32:11 -0300

    feat: refactor Footer component layout for improved responsiveness and organization

--- src/views/layout/Footer/Footer.tsx
+++ src/views/layout/Footer/Footer.tsx
@@ -6,7 +6,7 @@ import { Icon } from "@iconify/react/dist/iconify.js";
 export default function Footer() {
   return (
     <div className="flex w-full border-t border-zinc-700 bg-zinc-900">
-      <div className="mx-auto flex w-full max-w-screen-xl items-center justify-between px-4 py-20">
+      <div className="mx-auto flex w-full max-w-screen-xl flex-col items-center justify-between px-4 py-20 md:flex-row">
         <div className="flex flex-col gap-4">
           <Link href="/" className="flex items-center">
             <Logo className="max-w-32" />
@@ -20,32 +20,11 @@ export default function Footer() {
             </div>
           </div>
         </div>
-        <div className="flex flex-col items-center gap-4">
-          <div className="font-tight text-xl font-semibold">Solutions</div>
-          <div className="flex flex-col gap-2">
-            <Button variant="text">Small business</Button>
-            <Button variant="text">Freelancers</Button>
-            <Button variant="text">Costumers</Button>
-            <Button variant="text">Taxes</Button>
-          </div>
-        </div>
-        <div className="flex flex-col items-center gap-4">
-          <div className="font-tight text-xl font-semibold">Solutions</div>
-          <div className="flex flex-col gap-2">
-            <Button variant="text">Small business</Button>
-            <Button variant="text">Freelancers</Button>
-            <Button variant="text">Costumers</Button>
-            <Button variant="text">Taxes</Button>
-          </div>
-        </div>
-        <div className="flex flex-col items-center gap-4">
-          <div className="font-tight text-xl font-semibold">Solutions</div>
-          <div className="flex flex-col gap-2">
-            <Button variant="text">Small business</Button>
-            <Button variant="text">Freelancers</Button>
-            <Button variant="text">Costumers</Button>
-            <Button variant="text">Taxes</Button>
-          </div>
+        <div className="flex gap-2">
+          <Button variant="text">Small business</Button>
+          <Button variant="text">Freelancers</Button>
+          <Button variant="text">Costumers</Button>
+          <Button variant="text">Taxes</Button>
         </div>
         <div className="flex flex-col items-center">
           <div className="font-tight text-xl">Follow us on</div>

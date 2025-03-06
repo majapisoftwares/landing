63ba513c6d6e617d372466524db57934a84d1a96
caio <caio96.lemos@gmail.com>
2025-03-06 16:33:48 -0300

    feat: adjust layout in WhyChooseUs component for improved alignment and consistency

--- src/views/home/WhyChooseUs.tsx
+++ src/views/home/WhyChooseUs.tsx
@@ -9,7 +9,7 @@ export default function WhyChooseUs() {
   return (
     <div className="relative z-10 mx-auto flex w-full max-w-screen-xl flex-col gap-10 px-4">
       <div className="flex flex-col gap-12 lg:flex-row lg:gap-4">
-        <div className="flex w-full flex-col items-center justify-center gap-4 lg:items-start lg:gap-10">
+        <div className="flex w-full flex-col items-center justify-center gap-4 self-start lg:items-start lg:gap-10">
           <div className="bg-gradient-to-br from-zinc-100 to-zinc-400 bg-clip-text text-center font-dm text-3xl font-semibold text-transparent md:text-5xl md:tracking-[-2.88px] lg:text-left">
             Why choose us
           </div>
@@ -31,7 +31,7 @@ export default function WhyChooseUs() {
             </Button>
           </div>
         </div>
-        <div className="flex w-full flex-col lg:pt-12 gap-4">
+        <div className="flex w-full flex-col gap-4 lg:pt-12">
           <Accordion>
             <Accordion.Item
               className="rounded-lg border border-zinc-800 dark:bg-zinc-900 lg:mx-auto lg:w-[380px]"

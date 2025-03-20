69fb13cb418c6710fa8b06d29f58f5cad2599dfa
caio <caio96.lemos@gmail.com>
2025-03-20 14:52:08 -0300

    feat: enhance icon hover effects in Solutions component for improved interactivity

--- src/views/solutions/Solutions.tsx
+++ src/views/solutions/Solutions.tsx
@@ -21,19 +21,23 @@ const solutions = [
     icons: [
       <Icon
         icon="mdi:tailwind"
-        className="h-8 w-8 text-zinc-400"
+        className="h-8 w-8 text-zinc-400 hover:text-sky-500"
         key="tailwind"
       />,
-      <Icon icon="mdi:react" className="h-8 w-8 text-zinc-400" key="react" />,
+      <Icon
+        icon="mdi:react"
+        className="h-8 w-8 text-zinc-400 hover:text-sky-500"
+        key="react"
+      />,
       <Icon
         icon="nonicons:javascript-16"
-        className="h-8 w-8 text-zinc-400"
-        key="react"
+        className="h-8 w-8 text-zinc-400 hover:text-yellow-500"
+        key="javscript"
       />,
       <Icon
         icon="devicon-plain:nextjs"
-        className="h-8 w-8 text-zinc-400"
-        key="react"
+        className="h-8 w-8 text-zinc-400 hover:text-zinc-50"
+        key="nextjs"
       />,
     ],
     subtextOne: "Front-end (React / Next / Tailwind)",
@@ -49,8 +53,16 @@ const solutions = [
       "We craft intuitive interfaces and engaging experiences to maximize usability and user satisfaction. Strategic design for effective results.",
     img: uiuxdesign,
     icons: [
-      <Icon icon="gg:figma" className="h-8 w-8 text-zinc-400" key="figma" />,
-      <Icon icon="mage:photoshop" className="h-8 w-8 text-zinc-400" key="ps" />,
+      <Icon
+        icon="gg:figma"
+        className="h-8 w-8 text-zinc-400 hover:text-orange-500"
+        key="figma"
+      />,
+      <Icon
+        icon="mage:photoshop"
+        className="h-8 w-8 text-zinc-400 hover:text-sky-500"
+        key="ps"
+      />,
     ],
     subtextOne: "Website design",
     subtextTwo: "Mobile App Design",
@@ -65,11 +77,19 @@ const solutions = [
       "We create strong, memorable brands with unique visual identities. From logos to complete branding strategies, we ensure consistency and impact across all touchpoints.",
     img: billbords,
     icons: [
-      <Icon icon="gg:figma" className="h-8 w-8 text-zinc-400" key="figma" />,
-      <Icon icon="mage:photoshop" className="h-8 w-8 text-zinc-400" key="ps" />,
+      <Icon
+        icon="gg:figma"
+        className="h-8 w-8 text-zinc-400 hover:text-orange-500"
+        key="figma"
+      />,
+      <Icon
+        icon="mage:photoshop"
+        className="h-8 w-8 text-zinc-400 hover:text-sky-500"
+        key="ps"
+      />,
       <Icon
         icon="devicon-plain:illustrator"
-        className="h-7 w-7 text-zinc-400"
+        className="h-7 w-7 text-zinc-400 hover:text-red-900"
         key="illustrator"
       />,
     ],

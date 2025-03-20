520328f1a4bfb67d0440ecb32846e570885e083a
caio <caio96.lemos@gmail.com>
2025-03-20 11:01:55 -0300

    feat: add Vinicius Morais to team member profiles on About page

--- src/views/about/About.tsx
+++ src/views/about/About.tsx
@@ -9,6 +9,34 @@ import Ma from "../Ma";
 import italo from "./italo.webp";
 import cairo from "./cairo.webp";
 import caio from "./caio.webp";
+import vinicius from "./vinicius.webp";
+
+const staff = [
+  {
+    name: "Ítalo Andrade",
+    role: "Founder & CEO",
+    img: italo,
+    delay: 0.5,
+  },
+  {
+    name: "Cairo Andrade",
+    role: "Product Manager",
+    img: cairo,
+    delay: 0.75,
+  },
+  {
+    name: "Caio Lemos",
+    role: "Full Stack Júnior",
+    img: caio,
+    delay: 1,
+  },
+  {
+    name: "Vinicius Morais",
+    role: "Product Designer",
+    img: vinicius,
+    delay: 1.25,
+  },
+];
 
 export default function About() {
   return (
@@ -17,41 +45,45 @@ export default function About() {
       <Bg className="-top-12" />
       <div className="relative flex w-full flex-col gap-20 pt-20">
         <div className="relative mx-auto flex w-full max-w-screen-xl flex-col gap-10 px-4">
-          <Reveal delay={0.25}>
-            <div className="z-10 flex flex-col gap-4">
-              <div className="bg-gradient-to-br from-zinc-100 to-zinc-400 bg-clip-text text-center font-dm text-3xl font-semibold tracking-[-2.88px] text-transparent md:text-left md:text-5xl">
-                About us
-              </div>
-              <div className="max-w-[500px] self-center text-center font-dm text-lg leading-8 text-zinc-400 md:self-start md:text-left md:text-xl">
-                We are a technology-driven company dedicated to delivering
-                innovative and scalable solutions. With expertise in AI,
-                blockchain, and software development, we help businesses thrive
-                in a constantly evolving digital world.
-              </div>
+          <Reveal delay={0.25} className="z-10 flex flex-col gap-4">
+            <div className="bg-gradient-to-br from-zinc-100 to-zinc-400 bg-clip-text text-center font-dm text-3xl font-semibold tracking-[-2.88px] text-transparent md:text-left md:text-5xl">
+              About us
+            </div>
+            <div className="max-w-[500px] self-center text-center font-dm text-lg leading-8 text-zinc-400 md:self-start md:text-left md:text-xl">
+              We are a technology-driven company dedicated to delivering
+              innovative and scalable solutions. With expertise in AI,
+              blockchain, and software development, we help businesses thrive in
+              a constantly evolving digital world.
             </div>
           </Reveal>
           <div className="relative flex flex-col gap-4 p-6 md:flex-row">
-            <div className="absolute left-0 top-0 flex h-24 w-24 items-center justify-center rounded-xl bg-black">
+            <Reveal
+              delay={1}
+              className="absolute left-0 top-0 flex h-24 w-24 items-center justify-center rounded-xl bg-black"
+            >
               <Ma className="h-10 w-10" />
-            </div>
-            <div className="flex w-full">
+            </Reveal>
+            <Reveal delay={0.5} className="w-full">
               <Image className="w-full" src={office} alt="office" />
-            </div>
-            <div className="flex w-full">
+            </Reveal>
+            <Reveal delay={0.75} className="w-full">
               <Image className="w-full" src={work} alt="office" />
-            </div>
-            <div className="flex w-full">
+            </Reveal>
+            <Reveal delay={1} className="w-full">
               <Image className="w-full" src={workagain} alt="office" />
-            </div>
-            <div className="absolute bottom-0 right-0 flex h-24 w-24 items-center justify-center rounded-xl bg-zinc-50">
-              <Ma className="h-10 w-10 text-black" />
-            </div>
+              <Reveal
+                delay={1.5}
+                className="absolute bottom-0 right-0 flex h-24 w-24 items-center justify-center rounded-xl bg-zinc-50"
+              >
+                <Ma className="h-10 w-10 text-black" />
+              </Reveal>
+            </Reveal>
           </div>
           <div className="mb-20 flex w-full flex-col gap-16">
             <div className="relative">
               <Bg className="-top-44" />
             </div>
-            <div className="flex flex-col gap-4">
+            <Reveal delay={0.25} className="flex flex-col gap-4">
               <div className="bg-gradient-to-br from-zinc-100 to-zinc-400 bg-clip-text text-center font-dm text-3xl font-semibold tracking-[-2.88px] text-transparent md:text-5xl">
                 The Experts Behind the Innovation
               </div>
@@ -59,76 +91,31 @@ export default function About() {
                 A passionate team of experts dedicated to innovation,
                 creativity, and delivering exceptional solutions.
               </div>
-            </div>
+            </Reveal>
             <div className="z-10 grid grid-cols-2 gap-4 lg:grid-cols-4 lg:gap-24">
-              <div className="flex flex-col items-center justify-center gap-4 rounded-xl border border-zinc-800 bg-zinc-900 px-4 py-4">
-                <div className="flex">
-                  <Image
-                    className="w-full max-w-36 rounded-full"
-                    src={italo}
-                    alt="patrao"
-                  />
-                </div>
-                <div className="flex flex-col items-center justify-center gap-1">
-                  <div className="text-center font-dm text-xl text-zinc-50">
-                    Ítalo Andrade
+              {staff.map((member) => (
+                <Reveal
+                  delay={member.delay}
+                  key={member.name}
+                  className="flex flex-col items-center justify-center gap-4 rounded-xl border border-zinc-800 bg-zinc-900 px-4 py-4"
+                >
+                  <div className="flex">
+                    <Image
+                      className="w-full max-w-36 rounded-full"
+                      src={member.img}
+                      alt={member.name}
+                    />
                   </div>
-                  <div className="text-center font-dm text-lg text-zinc-50">
-                    Founder & CEO
+                  <div className="flex flex-col items-center justify-center gap-1">
+                    <div className="text-center font-dm text-xl text-zinc-50">
+                      {member.name}
+                    </div>
+                    <div className="text-center font-dm text-lg text-zinc-50">
+                      {member.role}
+                    </div>
                   </div>
-                </div>
-              </div>
-              <div className="flex flex-col items-center justify-center gap-4 rounded-xl border border-zinc-800 bg-zinc-900 px-4 py-4">
-                <div className="flex">
-                  <Image
-                    className="w-full max-w-36 rounded-full"
-                    src={cairo}
-                    alt="irmaodopatrao"
-                  />
-                </div>
-                <div className="flex flex-col items-center justify-center gap-1">
-                  <div className="text-center font-dm text-xl text-zinc-50">
-                    Cairo Andrade
-                  </div>
-                  <div className="text-center font-dm text-lg text-zinc-50">
-                    Product Manager
-                  </div>
-                </div>
-              </div>
-              <div className="flex flex-col items-center justify-center gap-4 rounded-xl border border-zinc-800 bg-zinc-900 px-4 py-4">
-                <div className="flex">
-                  <Image
-                    className="w-full max-w-36 rounded-full"
-                    src={caio}
-                    alt="patrao"
-                  />
-                </div>
-                <div className="flex flex-col items-center justify-center gap-1">
-                  <div className="text-center font-dm text-xl text-zinc-50">
-                    Caio Lemos
-                  </div>
-                  <div className="text-center font-dm text-lg text-zinc-50">
-                    Full Stack Júnior
-                  </div>
-                </div>
-              </div>
-              <div className="flex flex-col items-center justify-center gap-4 rounded-xl border border-zinc-800 bg-zinc-900 px-4 py-4">
-                <div className="flex">
-                  <Image
-                    className="w-full max-w-36 rounded-full"
-                    src={italo}
-                    alt="patrao"
-                  />
-                </div>
-                <div className="flex flex-col items-center justify-center gap-1">
-                  <div className="text-center font-dm text-xl text-zinc-50">
-                    Vini Jr
-                  </div>
-                  <div className="text-center font-dm text-lg text-zinc-50">
-                    Product Designer
-                  </div>
-                </div>
-              </div>
+                </Reveal>
+              ))}
             </div>
           </div>
         </div>
